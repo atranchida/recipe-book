e72e6f7dee2fece79928deabf1107dd44e14ef6a
atranchida <atranchida@performair.com>
2020-04-02 15:26:06 -0700

    creates unit test for RecipeCard component

---
+++ src/__tests__/RecipeCard.test.tsx
@@ -0,0 +1,38 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render, fireEvent } from "@testing-library/react";
+import React from "react";
+import RecipeCard, { Props } from "../components/RecipeCard";
+import { Recipe } from "../interfaces/Recipe";
+
+const mockRecipe: Recipe = {
+    name: "Apple Pie",
+    ingredients: [
+        {
+            "name": "Apples"
+        }
+    ]
+};
+
+function renderRecipeCard({ ...props }: Partial<Props> = {}) {
+    const defaultProps: Props = {
+
+        recipe: mockRecipe,
+
+        onDelete() {
+            return;
+        }
+    };
+    return render(<RecipeCard {...defaultProps} {...props} />);
+}
+
+describe("<RecipeCard />", () => {
+    test("should allow deleting of a recipe when delete button is clicked", async () => {
+        const onDelete = jest.fn();
+        const { findByTestId } = renderRecipeCard({ recipe: mockRecipe, onDelete });
+        const deleteRecipe = await findByTestId("delete-recipe");
+
+        fireEvent.click(deleteRecipe);
+
+        expect(onDelete).toHaveBeenCalledWith(mockRecipe.name);
+    });
+});
--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -3,19 +3,24 @@ import { Recipe } from "../interfaces/Recipe";
 import IngredientList from "./IngredientList";
 import styles from '../css/RecipeList.module.css';
 
-interface Props {
+export interface Props {
     recipe: Recipe;
     onDelete: (name: string) => void
 }
 
 function RecipeCard({ recipe, onDelete }: Props) {
-    return (<div className={styles.RecipeCard}> 
-        <button className={styles.DeleteButton} onClick={() => onDelete(recipe.name)}>X</button>
-        <div className={styles.CardContents}>
-            <h1>{recipe.name}</h1>
-            <IngredientList ingredients={recipe.ingredients} />
-        </div>
-    </div>)
+    return (
+        <div className={styles.RecipeCard}>>
+            <button className={styles.DeleteButton}
+                onClick={() => onDelete(recipe.name)}
+                data-testid="delete-recipe">
+                >X
+            </button>
+            <div className={styles.CardContents}>
+                <h1>{recipe.name}</h1>
+                <IngredientList ingredients={recipe.ingredients} />
+            </div>
+        </div>)
 }
 
 export default RecipeCard;
