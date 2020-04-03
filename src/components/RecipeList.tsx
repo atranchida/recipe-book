e8a9d07e89c043d52e6fa67dc8c06a459238049c
Connie Catedral <crcatedr@asu.edu>
2020-04-03 12:42:20 -0700

    starts trying to implement infinite scroller

--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import styles from '../css/RecipeList.module.css';
 import { Recipe } from "../interfaces/Recipe";
 import RecipeCard from "./RecipeCard";
-import styles from '../css/RecipeList.module.css';
 
 interface Props {
     recipes: Recipe[];
@@ -9,9 +10,33 @@ interface Props {
 }
 
 const RecipeList = ({ recipes, onDelete }: Props) => {
+    const [currentRecipes, setCurrentRecipes] = useState<Array<Recipe>>([]);
+    const [hasMore, setHasMore] = useState(true);
+
+    function fetchMoreData() {
+        if(currentRecipes.length >= 50){
+            setHasMore(false);
+            return;
+        }
+        setCurrentRecipes(currentRecipes.concat(recipes.slice(4,50)))
+    };
+
+    useEffect(() => {
+        let testArray = Array.from(recipes.splice(0,2)); //trying to load the first 2 recipes
+        setCurrentRecipes(testArray)
+    }, [])
+  
     return (
         <div data-testid="recipeList" className = {styles.RecipeList}>
-            {recipes.map(recipe => (
+            
+            <span>{currentRecipes.length}</span>
+            <InfiniteScroll
+                dataLength={recipes.length}
+                next={fetchMoreData}
+                hasMore={hasMore}
+                loader={<h4>Loading...</h4>}
+            >
+                {currentRecipes.map(recipe => (
                 <RecipeCard
                     key={recipe.name}
                     recipe={{
@@ -21,6 +46,7 @@ const RecipeList = ({ recipes, onDelete }: Props) => {
                    onDelete = {onDelete}
                 />
             ))}
+            </InfiniteScroll>
         </div>
     );
 };
