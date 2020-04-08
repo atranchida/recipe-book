aa79b86c00fbaa83063195cc43bc96721563152b
atranchida <atranchida@performair.com>
2020-04-08 09:00:37 -0700

    adds contenteditable for editing recipes

--- src/components/IngredientList.tsx
+++ src/components/IngredientList.tsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Ingredient } from "../interfaces/Ingredient";
-import IngredientListItem from "./IngredientListItem";
+import ContentEditable from "react-contenteditable";
+
 
 interface Props {
     ingredients: Ingredient[];
 }
 
 const IngredientList = ({ ingredients }: Props) => {
+
+    const text = useRef("");
+
+    const handleChange = (evt: { target: { value: string; }; }) => {
+        text.current = evt.target.value;
+    };
+
+    const handleBlur = () => {
+        console.log(text.current);
+    };
+
     return (
         <div>
             <ul>
                 {ingredients.map(ingredient => (
-                    <IngredientListItem
+                    <ContentEditable
                         key={ingredient.name}
-                        ingredient={{
-                            name: ingredient.name
-                        }}
+                        html={ingredient.name}
+                        onBlur={handleBlur}
+                        onChange={handleChange}
                     />
                 ))}
             </ul>
--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Recipe } from "../interfaces/Recipe";
 import IngredientList from "./IngredientList";
 import styles from '../css/RecipeList.module.css';
+import ContentEditable from 'react-contenteditable'
 
 export interface Props {
     recipe: Recipe;
@@ -9,18 +10,38 @@ export interface Props {
 }
 
 function RecipeCard({ recipe, onDelete }: Props) {
+
+    const text = useRef("");
+    const [html, setHTML] = useState(`<h1>` + recipe.name + `</h1>`);
+
+    const handleChange = (evt: { target: { value: string; }; }) => {
+        text.current = evt.target.value;
+        setHTML(text.current);
+    };
+
+    const handleBlur = () => {
+        console.log(text.current);
+    };
+
     return (
-        <div className={styles.RecipeCard}>
-            <button className={styles.DeleteButton}
-                onClick={() => onDelete(recipe.name)}
-                data-testid="delete-recipe">
-                X
+        <>
+            <div className={styles.RecipeCard}>
+                <button className={styles.DeleteButton}
+                    onClick={() => onDelete(recipe.name)}
+                    data-testid="delete-recipe">
+                    X
             </button>
-            <div className={styles.CardContents}>
-                <h1>{recipe.name}</h1>
-                <IngredientList ingredients={recipe.ingredients} />
+                <div className={styles.CardContents}>
+                    <ContentEditable
+                        html={html}
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                    />
+                    <IngredientList ingredients={recipe.ingredients} />
+                </div>
             </div>
-        </div>)
+        </>
+    )
 }
 
 export default RecipeCard;
