47b27875653f8c4098743a24b18bce89e0d82166
atranchida <atranchida@performair.com>
2020-04-13 13:19:07 -0700

    adds addRecipe and setRecipes to actions.ts and moves old stuff to Recipes.ts

--- src/interfaces/Recipe.ts
+++ src/interfaces/Recipe.ts
@@ -1,6 +1,52 @@
-import { Ingredient } from "./Ingredient";
+import { Ingredient } from "../interfaces/Ingredient"
 
 export interface Recipe {
     name: string;
     ingredients: Ingredient[];
 }
+
+export interface RecipeBookState {
+    recipes: Recipe[]
+}
+
+export const ADD_RECIPE = "ADD_RECIPE"
+ export const SET_RECIPES = "SET_RECIPES"
+
+// export const EDIT_RECIPE = "EDIT_RECIPE"
+// export const EDIT_INGREDIENT = "EDIT_INGREDIENT"
+// export const DELETE_RECIPE = "DELETE_RECIPE"
+
+
+export interface SetRecipeAction {
+    type: typeof SET_RECIPES;
+    recipes: Recipe[];
+}
+
+export interface AddRecipeAction {
+    type: typeof ADD_RECIPE;
+    recipe: Recipe;
+}
+
+// export interface EditRecipeAction {
+//     type: typeof EDIT_RECIPE;
+//     recipe: Recipe;
+// }
+
+// export interface DeleteRecipeAction {
+//     type: typeof DELETE_RECIPE;
+//     recipe: Recipe;
+// }
+
+// export interface EditIngredientAction {
+//     type: typeof EDIT_INGREDIENT;
+//     ingredient: Ingredient;
+// }
+
+export type RecipeActionTypes =
+    | SetRecipeAction
+    | AddRecipeAction
+    // | EditRecipeAction
+    // | DeleteRecipeAction
+    // | EditIngredientAction
+
+export type AppActions = RecipeActionTypes
--- src/types/actions.ts
+++ src/types/actions.ts
@@ -1,44 +1,16 @@
-import { Recipe } from "../interfaces/Recipe"
-import { Ingredient } from "../interfaces/Ingredient"
+import { ADD_RECIPE, Recipe, RecipeActionTypes, SET_RECIPES } from "../interfaces/Recipe";
 
-export const ADD_RECIPE = "ADD_RECIPE"
-export const EDIT_RECIPE = "EDIT_RECIPE"
-export const EDIT_INGREDIENT = "EDIT_INGREDIENT"
-export const DELETE_RECIPE = "DELETE_RECIPE"
-export const SET_RECIPES = "SET_RECIPES"
+export const addRecipe = ( recipe: Recipe ): RecipeActionTypes => ({
+        type: ADD_RECIPE,
+        recipe
+});
 
-export interface SetRecipeAction {
-    type: typeof SET_RECIPES;
-    recipes: Recipe[];
-}
+export const setRecipes = ( recipes: Recipe[] ): RecipeActionTypes => ({
+    type: SET_RECIPES,
+    recipes
+});
 
-export interface AddRecipeAction {
-    type: typeof ADD_RECIPE;
-    recipe: Recipe;
-}
-
-export interface EditRecipeAction {
-    type: typeof EDIT_RECIPE;
-    recipe: Recipe;
-}
-
-export interface DeleteRecipeAction {
-    type: typeof DELETE_RECIPE;
-    recipe: Recipe;
-}
-
-// Don't know if this is needed
-export interface EditIngredientAction {
-    type: typeof EDIT_INGREDIENT;
-    ingredient: Ingredient;
-}
-
-export type RecipeActionTypes =
-    | SetRecipeAction
-    | AddRecipeAction
-    | EditRecipeAction
-    | DeleteRecipeAction
-    | EditIngredientAction
-
-// can add filter action types somehow
-export type AppActions = RecipeActionTypes
+//   const handleAddRecipe = (newRecipe: Recipe) => {
+//     var newRecipes = [newRecipe, ...recipes]
+//     setRecipes(newRecipes);
+//   };
